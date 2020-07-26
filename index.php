a9628a4320cbbe5425e05469f438e4bb6f3ea902
lucashca23 <68822150+lucashca23@users.noreply.github.com>
2020-07-26 14:44:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+set_time_limit(0);
+
+include 'vendor/autoload.php';
+
+$qtdVotos = 100000;
+
+$array = [
+    'content_id' => 'xws131gz',
+    'checked_answers' => [
+        '762rgcyf5zr1'
+    ],
+    'name' => null
+];
+
+for ($contador = 0; $contador <= $qtdVotos; $contador++)
+{
+    try
+    {
+        $response = \Httpful\Request::post('https://strawpoll.com/api/poll/vote')
+                    //->useProxy('128.199.246.10', 44344)
+                    ->expectsJson()
+                    ->body($array)
+                    ->sendsJson()
+                    ->send();
+        //$objJson = $response->body;
+        //echo 'Voto '.$contador.' = '.$objJson->message.'<br>';
+    }
+    catch(Exception $e)
+    {
+        //echo 'Falha! Voto '.$contador.' = Negado pelo servidor proxy<br>';    
+    }
+}
